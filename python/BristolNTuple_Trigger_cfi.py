b0b98662c9533ca043d4e9fff308990d1d146291
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-05-28 19:06:24 +0000

    updated trigger list for 2011

--- python/BristolNTuple_Trigger_cfi.py
+++ python/BristolNTuple_Trigger_cfi.py
@@ -19,13 +19,36 @@ rootTupleTrigger = cms.EDProducer("BristolNTuple_Trigger",
     'HLT_Ele17_SW_TighterEleId_L1R_v1', #9
     'HLT_Ele22_SW_TighterEleId_L1R_v', #10
     'HLT_Ele27_SW_TightCaloEleIdTrack_L1R_v1', #11
-    #2011 data  
-    'HLT_Ele25_CaloIdVT_TrkIdT_CentralDiJet30_v', #12
+    #========== 2011 data ==============  
+    #ElectronHad PD
+    'HLT_Ele25_CaloIdVT_TrkIdT_CentralJet40_BTagIP_v', #12
     'HLT_Ele25_CaloIdVT_TrkIdT_CentralJet30_v', #13
-    'HLT_Ele25_CaloIdVT_TrkIdT_CentralJet40_BTagIP_v', #14
+    'HLT_Ele25_CaloIdVT_TrkIdT_CentralDiJet30_v', #14
     'HLT_Ele25_CaloIdVT_TrkIdT_CentralTriJet30_v', #15
-    'HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v', #16
-    'HLT_Ele45_CaloIdVT_TrkIdT_v', #17
+    #renaming for 1E33
+    'HLT_Ele25_CaloIdVT_TrkIdT_CentralJet30_BTagIP_v', #16
+    'HLT_Ele25_CaloIdVT_TrkIdT_DiCentralJet30_v',#17
+    'HLT_Ele25_CaloIdVT_TrkIdT_TriCentralJet30_v', #18
+    'HLT_Ele25_CaloIdVT_TrkIdT_QuadCentralJet30_v', #19
+    #new iso triggers:
+    'HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralJet30_BTagIP_v',#20
+    'HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralJet30_v',#21
+    'HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_DiCentralJet30_v',#22
+    'HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_TriCentralJet30_v',#23
+    'HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_QuadCentralJet30_v',#24
+    #Higgs trigger
+    'HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralJet30_CentralJet25_PFMHT20_v',#25
+    #EWK triggers
+    'HLT_Ele25_WP80_PFMT40_v',#26
+    'HLT_Ele27_WP70_PFMT40_PFMHT20_v'#27
+    
+    
+    #SingleElectron PD
+    'HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v', #28
+    'HLT_Ele25_CaloIdL_CaloIsoVL_TrkIdVL_TrkIsoVL_v',#29
+    'HLT_Ele32_CaloIdVL_CaloIsoVL_TrkIdVL_TrkIsoVL_v',#30
+    'HLT_Ele32_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v',#31
+    
  ),                              
     HLTPathsOfInterest_Other = cms.vstring(
 #ElectronHad
