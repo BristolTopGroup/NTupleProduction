ddd95fd6c13f72cbc28cf0da202210ad2a6c5650
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-09-23 07:57:00 +0000

    fixing prefix for GenMET

--- python/BristolNTuple_GenMET_cfi.py
+++ python/BristolNTuple_GenMET_cfi.py
@@ -2,15 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 rootTupleGenMETTrue = cms.EDProducer( "BristolNTuple_GenMET",
     InputTag = cms.InputTag( 'genMetTrue' ),
-    Prefix = cms.string( 'Gen' ),
+    Prefix = cms.string( 'GenMET.' ),
     Suffix = cms.string( 'True' )
  )
 
 rootTupleGenMETCalo = cms.EDProducer( "BristolNTuple_GenMET",
     InputTag = cms.InputTag( 'genMetCalo' ),
-    Prefix = cms.string( 'Gen' ),
+    Prefix = cms.string( 'GenMET.' ),
     Suffix = cms.string( 'Calo' )
  )
-
-rootTupleGenMETSequence = cms.Sequence( rootTupleGenMETTrue)
-
