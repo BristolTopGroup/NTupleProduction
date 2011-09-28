e24da13bf1f3bdd2764dd7efe265f7fa5987dbef
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-09-28 21:43:40 +0000

    removed eta, phi and Pt and added mass

--- src/BristolNTuple_GenParticles.cc
+++ src/BristolNTuple_GenParticles.cc
@@ -10,14 +10,12 @@ BristolNTuple_GenParticles::BristolNTuple_GenParticles(const edm::ParameterSet& 
     suffix  (iConfig.getParameter<std::string>  ("Suffix")),
     maxSize (iConfig.getParameter<unsigned int> ("MaxSize"))
 {
-  produces <std::vector<double> > ( prefix + "Eta" + suffix );
-  produces <std::vector<double> > ( prefix + "Phi" + suffix );
-//  produces <std::vector<double> > ( prefix + "P" + suffix );
   produces <std::vector<double> > ( prefix + "Px" + suffix );
   produces <std::vector<double> > ( prefix + "Py" + suffix );
   produces <std::vector<double> > ( prefix + "Pz" + suffix );
-  produces <std::vector<double> > ( prefix + "Pt" + suffix );
   produces <std::vector<double> > ( prefix + "Energy" + suffix );
+  produces <std::vector<double> > ( prefix + "Charge" + suffix );
+  produces <std::vector<double> > ( prefix + "Mass" + suffix );
   produces <std::vector<int> >    ( prefix + "PdgId" + suffix );
   produces <std::vector<double> > ( prefix + "VX" + suffix );
   produces <std::vector<double> > ( prefix + "VY" + suffix );
@@ -30,80 +28,75 @@ BristolNTuple_GenParticles::BristolNTuple_GenParticles(const edm::ParameterSet& 
 void BristolNTuple_GenParticles::
 produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
-  std::auto_ptr<std::vector<double> >  eta  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> >  phi  ( new std::vector<double>()  );
-//  std::auto_ptr<std::vector<double> >  p  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> >  px  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> >  py  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> >  pz  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> >  pt  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> >  energy  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<int> >     pdgId ( new std::vector<int>()  );
-  std::auto_ptr<std::vector<double> >  vx  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> >  vy  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<double> >  vz  ( new std::vector<double>()  );
-  std::auto_ptr<std::vector<int> >     numDaught  ( new std::vector<int>()  );
-  std::auto_ptr<std::vector<int> >     status  ( new std::vector<int>()  );
-  std::auto_ptr<std::vector<int> >     motherIndex  ( new std::vector<int>()  );
-  
-  //-----------------------------------------------------------------
-  if( !iEvent.isRealData() ) {
-    edm::Handle<reco::GenParticleCollection> genParticles;
-    iEvent.getByLabel(inputTag, genParticles);
+	std::auto_ptr < std::vector<double> > px(new std::vector<double>());
+	std::auto_ptr < std::vector<double> > py(new std::vector<double>());
+	std::auto_ptr < std::vector<double> > pz(new std::vector<double>());
+	std::auto_ptr < std::vector<double> > energy(new std::vector<double>());
+	std::auto_ptr < std::vector<double> > charge(new std::vector<double>());
+	std::auto_ptr < std::vector<double> > mass(new std::vector<double>());
+	std::auto_ptr < std::vector<int> > pdgId(new std::vector<int>());
+	std::auto_ptr < std::vector<double> > vx(new std::vector<double>());
+	std::auto_ptr < std::vector<double> > vy(new std::vector<double>());
+	std::auto_ptr < std::vector<double> > vz(new std::vector<double>());
+	std::auto_ptr < std::vector<int> > numDaught(new std::vector<int>());
+	std::auto_ptr < std::vector<int> > status(new std::vector<int>());
+	std::auto_ptr < std::vector<int> > motherIndex(new std::vector<int>());
 
-    if( genParticles.isValid() ) {
-      edm::LogInfo("BristolNTuple_GenParticlesInfo") << "Total # GenParticles: " << genParticles->size();
+	//-----------------------------------------------------------------
+	if (!iEvent.isRealData()) {
+		edm::Handle < reco::GenParticleCollection > genParticles;
+		iEvent.getByLabel(inputTag, genParticles);
 
-      for( reco::GenParticleCollection::const_iterator it = genParticles->begin(); it != genParticles->end(); ++it ) {
-        // exit from loop when you reach the required number of GenParticles
-        if(eta->size() >= maxSize)
-          break;
+		if (genParticles.isValid()) {
+			edm::LogInfo("BristolNTuple_GenParticlesInfo") << "Total # GenParticles: " << genParticles->size();
 
-        // fill in all the vectors
-        eta->push_back( it->eta() );
-        phi->push_back( it->phi() );
-//        p->push_back( it->p() );
-        px->push_back( it->px() );
-        py->push_back( it->py() );
-        pz->push_back( it->pz() );
-        pt->push_back( it->pt() );
-        energy->push_back( it->energy() );
-        pdgId->push_back( it->pdgId() );
-        vx->push_back( it->vx() );
-        vy->push_back( it->vy() );
-        vz->push_back( it->vz() );
-        numDaught->push_back( it->numberOfDaughters() );
-        status->push_back( it->status() );
+			for (reco::GenParticleCollection::const_iterator it = genParticles->begin(); it != genParticles->end();
+					++it) {
+				// exit from loop when you reach the required number of GenParticles
+				if (px->size() >= maxSize)
+					break;
 
-        int idx = -1;
-        for (reco::GenParticleCollection::const_iterator mit = genParticles->begin(); mit
-                        != genParticles->end(); ++mit) {
-                    if (it->mother() == &(*mit)) {
-                        idx = std::distance(genParticles->begin(), mit);
-                        break;
-                    }
-                }
-        motherIndex->push_back( idx );
-      }
-    } else {
-      edm::LogError("BristolNTuple_GenParticlesError") << "Error! Can't get the product " << inputTag;
-    }
-  }
-  //-----------------------------------------------------------------
-  // put vectors in the event
-  iEvent.put( eta, prefix + "Eta" + suffix );
-  iEvent.put( phi, prefix + "Phi" + suffix );
-//  iEvent.put( p, prefix + "P" + suffix );
-  iEvent.put( px, prefix + "Px" + suffix );
-  iEvent.put( py, prefix + "Py" + suffix );
-  iEvent.put( pz, prefix + "Pz" + suffix );
-  iEvent.put( pt, prefix + "Pt" + suffix );
-  iEvent.put( energy, prefix + "Energy" + suffix );
-  iEvent.put( pdgId, prefix + "PdgId" + suffix );
-  iEvent.put( vx, prefix + "VX" + suffix );
-  iEvent.put( vy, prefix + "VY" + suffix );
-  iEvent.put( vz, prefix + "VZ" + suffix );
-  iEvent.put( numDaught, prefix + "NumDaught" + suffix );
-  iEvent.put( status, prefix + "Status" + suffix );
-  iEvent.put( motherIndex, prefix + "MotherIndex" + suffix );
+				// fill in all the vectors
+				px->push_back(it->px());
+				py->push_back(it->py());
+				pz->push_back(it->pz());
+				energy->push_back(it->energy());
+				pdgId->push_back(it->pdgId());
+				vx->push_back(it->vx());
+				vy->push_back(it->vy());
+				vz->push_back(it->vz());
+				numDaught->push_back(it->numberOfDaughters());
+				status->push_back(it->status());
+				charge->push_back(it->charge());
+				mass->push_back(it->mass());
+
+				int idx = -1;
+				for (reco::GenParticleCollection::const_iterator mit = genParticles->begin();
+						mit != genParticles->end(); ++mit) {
+					if (it->mother() == &(*mit)) {
+						idx = std::distance(genParticles->begin(), mit);
+						break;
+					}
+				}
+				motherIndex->push_back(idx);
+			}
+		} else {
+			edm::LogError("BristolNTuple_GenParticlesError") << "Error! Can't get the product " << inputTag;
+		}
+	}
+	//-----------------------------------------------------------------
+	// put vectors in the event
+	iEvent.put(px, prefix + "Px" + suffix);
+	iEvent.put(py, prefix + "Py" + suffix);
+	iEvent.put(pz, prefix + "Pz" + suffix);
+	iEvent.put(energy, prefix + "Energy" + suffix);
+	iEvent.put(charge, prefix + "Charge" + suffix);
+	iEvent.put(mass, prefix + "Mass" + suffix);
+	iEvent.put(pdgId, prefix + "PdgId" + suffix);
+	iEvent.put(vx, prefix + "VX" + suffix);
+	iEvent.put(vy, prefix + "VY" + suffix);
+	iEvent.put(vz, prefix + "VZ" + suffix);
+	iEvent.put(numDaught, prefix + "NumDaught" + suffix);
+	iEvent.put(status, prefix + "Status" + suffix);
+	iEvent.put(motherIndex, prefix + "MotherIndex" + suffix);
 }
