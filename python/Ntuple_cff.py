a23885dbc2f2a7ca8b7791fc5f913dd05c9deaa0
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-09-29 14:17:45 +0000

    removed JPT jets from config

--- python/Ntuple_cff.py
+++ python/Ntuple_cff.py
@@ -4,7 +4,6 @@ from BristolAnalysis.NTupleTools.BristolNTuple_BeamSpot_cfi import *
 from BristolAnalysis.NTupleTools.BristolNTuple_Event_cfi import *
 from BristolAnalysis.NTupleTools.BristolNTuple_CaloJets_cfi import *
 from BristolAnalysis.NTupleTools.BristolNTuple_PFJets_cfi import *
-from BristolAnalysis.NTupleTools.BristolNTuple_JPTJets_cfi import *
 from BristolAnalysis.NTupleTools.BristolNTuple_Electrons_cfi import *
 from BristolAnalysis.NTupleTools.BristolNTuple_MET_cfi import *
 from BristolAnalysis.NTupleTools.BristolNTuple_Muons_cfi import *
