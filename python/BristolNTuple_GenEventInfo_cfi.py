8eaaf308f4f095de329ae808a85ec9ad1f3ac52f
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2011-12-31 13:59:46 +0000

    adding input for new parameters

--- python/BristolNTuple_GenEventInfo_cfi.py
+++ python/BristolNTuple_GenEventInfo_cfi.py
@@ -3,8 +3,12 @@ import FWCore.ParameterSet.Config as cms
 rootTupleGenEventInfo = cms.EDProducer("BristolNTuple_GenEventInfo",
     GenEventInfoInputTag = cms.InputTag('generator'),
     StorePDFWeights      = cms.bool(False),
+    isFall11MC           = cms.bool(False),
     PDFWeightsInputTag   = cms.InputTag('pdfWeights','cteq65'),
     pileupInfo           = cms.InputTag('addPileupInfo'),
-    Prefix=cms.string('Event.'),
-    Suffix=cms.string('')
+    Prefix               = cms.string('Event.'),
+    Suffix               = cms.string(''),
+    dataPileUpFile       = cms.string('BristolAnalysis/NTupleTools/data/PileUp_2011_truth_finebin.root'),
+    PossionShiftUp       = cms.double(0.5),
+    PossionShiftDown       = cms.double(-0.5),
 )
