fa5847b9e420bbf66c34906c0b1b83865e9df64b
kreczko <kreczko@4525493e-7705-40b1-a816-d608a930855b>
2012-01-09 11:03:19 +0000

    added new JEC

--- test/nTupleMaker_cfg.py
+++ test/nTupleMaker_cfg.py
@@ -60,6 +60,32 @@ options.register ('skim',
 
 options.parseArguments()
 
+#--------------------------------------------------------------------------------
+#
+# configure Jet Energy Corrections
+#
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.jec = cms.ESSource("PoolDBESSource",
+   DBParameters = cms.PSet(
+       messageLevel = cms.untracked.int32(0)
+   ),
+   timetype = cms.string('runnumber'),
+   toGet = cms.VPSet(
+       cms.PSet(
+           record = cms.string('JetCorrectionsRecord'),
+           tag    = cms.string('JetCorrectorParametersCollection_Jec11_V12_AK5PF'),
+           label  = cms.untracked.string('AK5PF')
+       ),
+       cms.PSet(
+           record = cms.string('JetCorrectionsRecord'),
+           tag    = cms.string('JetCorrectorParametersCollection_Jec11_V12_AK5Calo'),
+           label  = cms.untracked.string('AK5Calo')
+       )
+   ),
+   connect = cms.string('sqlite_fip:Jec11_V12_20111220.db')
+)
+process.es_prefer_jec = cms.ESPrefer('PoolDBESSource', 'jec')
+
 
 if not options.useData :
     inputJetCorrLabel = ('AK5PFchs', ['L1FastJet', 'L2Relative', 'L3Absolute'])
