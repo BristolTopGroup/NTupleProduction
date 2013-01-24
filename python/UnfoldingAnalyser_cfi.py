d26ee473938c2191b614cf73845fddf0501b0d7b
senkin <s.senkin@gmail.com>
2013-01-24 12:27:52 +0000

    Adding additional METs in the unfolding

--- python/UnfoldingAnalyser_cfi.py
+++ python/UnfoldingAnalyser_cfi.py
@@ -1,10 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
-unfolding_MET_analyser_electron_channel = cms.EDAnalyzer("UnfoldingAnalyser",
+unfolding_MET_analyser_electron_channel_patMETsPFlow = cms.EDAnalyzer("UnfoldingAnalyser",
     pu_weight_input=cms.InputTag('eventWeightPU'),
     b_tag_weight_input=cms.InputTag('eventWeightBtag'),
     gen_MET_input=cms.InputTag('genMetTrue'),
-    reco_MET_Input=cms.InputTag('patType1CorrectedPFMet'),
+    reco_MET_Input=cms.InputTag('patMETsPFlow'),
     gen_jet_input=cms.InputTag('ak5GenJets'),
     reco_jet_input=cms.InputTag('selectedPatJetsPFlow'),
     electron_input=cms.InputTag("topPairEPlusJetsSelection", 'TopPairElectronPlusJets2012Selection.signalElectron', 'PAT'),
@@ -25,11 +25,11 @@ unfolding_MET_analyser_electron_channel = cms.EDAnalyzer("UnfoldingAnalyser",
     bin_edges=cms.vdouble([0, 25, 45, 70, 100, 150, 2000])
 )
 
-unfolding_MET_analyser_muon_channel = unfolding_MET_analyser_electron_channel.clone(
+unfolding_MET_analyser_muon_channel_patMETsPFlow = unfolding_MET_analyser_electron_channel_patMETsPFlow.clone(
     do_electron_channel=cms.untracked.bool(False),
 )
 
-unfolding_HT_analyser_electron_channel = unfolding_MET_analyser_electron_channel.clone(
+unfolding_HT_analyser_electron_channel = unfolding_MET_analyser_electron_channel_patMETsPFlow.clone(
     variable_under_analysis=cms.string('HT'),
     variable_min=cms.double(50),
     variable_max=cms.double(2000.),
@@ -41,7 +41,7 @@ unfolding_HT_analyser_muon_channel = unfolding_HT_analyser_electron_channel.clon
     do_electron_channel=cms.untracked.bool(False),
 )
 
-unfolding_ST_analyser_electron_channel = unfolding_MET_analyser_electron_channel.clone(
+unfolding_ST_analyser_electron_channel_patMETsPFlow = unfolding_MET_analyser_electron_channel_patMETsPFlow.clone(
     variable_under_analysis=cms.string('ST'),
     variable_min=cms.double(150),
     variable_max=cms.double(2000.),
@@ -49,11 +49,11 @@ unfolding_ST_analyser_electron_channel = unfolding_MET_analyser_electron_channel
     bin_edges=cms.vdouble([150, 250, 350, 450, 550, 750, 1250, 2000])
 )
 
-unfolding_ST_analyser_muon_channel = unfolding_HT_analyser_electron_channel.clone(
+unfolding_ST_analyser_muon_channel_patMETsPFlow = unfolding_ST_analyser_electron_channel_patMETsPFlow.clone(
     do_electron_channel=cms.untracked.bool(False),
 )
 
-unfolding_MT_analyser_electron_channel = unfolding_MET_analyser_electron_channel.clone(
+unfolding_MT_analyser_electron_channel_patMETsPFlow = unfolding_MET_analyser_electron_channel_patMETsPFlow.clone(
     variable_under_analysis=cms.string('MT'),
     variable_min=cms.double(0),
     variable_max=cms.double(1000.),
@@ -61,6 +61,30 @@ unfolding_MT_analyser_electron_channel = unfolding_MET_analyser_electron_channel
     bin_edges=cms.vdouble([0, 40, 65, 85, 150, 1000])
 )
 
-unfolding_MT_analyser_muon_channel = unfolding_HT_analyser_electron_channel.clone(
+unfolding_MT_analyser_muon_channel_patMETsPFlow = unfolding_MT_analyser_electron_channel_patMETsPFlow.clone(
     do_electron_channel=cms.untracked.bool(False),
+)
+
+unfolding_MET_analyser_electron_channel_patType1CorrectedPFMet = unfolding_MET_analyser_electron_channel_patMETsPFlow.clone(
+    reco_MET_Input=cms.InputTag('patType1CorrectedPFMet'),
+)
+
+unfolding_MET_analyser_muon_channel_patType1CorrectedPFMet = unfolding_MET_analyser_muon_channel_patMETsPFlow.clone(
+    reco_MET_Input=cms.InputTag('patType1CorrectedPFMet'),
+)
+
+unfolding_ST_analyser_electron_channel_patType1CorrectedPFMet = unfolding_ST_analyser_electron_channel_patMETsPFlow.clone(
+    reco_MET_Input=cms.InputTag('patType1CorrectedPFMet'),
+)
+
+unfolding_ST_analyser_muon_channel_patType1CorrectedPFMet = unfolding_ST_analyser_muon_channel_patMETsPFlow.clone(
+    reco_MET_Input=cms.InputTag('patType1CorrectedPFMet'),
+)
+
+unfolding_MT_analyser_electron_channel_patType1CorrectedPFMet = unfolding_MT_analyser_electron_channel_patMETsPFlow.clone(
+    reco_MET_Input=cms.InputTag('patType1CorrectedPFMet'),
+)
+
+unfolding_MT_analyser_muon_channel_patType1CorrectedPFMet = unfolding_MT_analyser_muon_channel_patMETsPFlow.clone(
+    reco_MET_Input=cms.InputTag('patType1CorrectedPFMet'),
 )
