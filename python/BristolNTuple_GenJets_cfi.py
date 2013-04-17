8d708f6733cf289a75ddcd15490c09d80a4bef15
kreczko <lkreczko@gmail.com>
2013-04-17 09:57:20 +0100

    remove gen jet cuts for ntuples (BristolTopGroup/NTupleProduction/#38)

--- python/BristolNTuple_GenJets_cfi.py
+++ python/BristolNTuple_GenJets_cfi.py
@@ -4,8 +4,8 @@ rootTupleGenJets = cms.EDProducer("BristolNTuple_GenJets",
     InputTag = cms.InputTag('ak5GenJetsNoNu'),
     Prefix = cms.string('GenJet.'),
     Suffix = cms.string(''),
-    minPt = cms.double(15.),
-    maxAbsoluteEta = cms.double(2.6),
+    minPt = cms.double(-1),
+    maxAbsoluteEta = cms.double(999),
     MaxSize = cms.uint32(99)
 )
 
