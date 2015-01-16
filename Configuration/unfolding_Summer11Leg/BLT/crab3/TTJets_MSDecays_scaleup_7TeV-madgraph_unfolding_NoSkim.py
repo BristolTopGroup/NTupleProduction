5597775c5a6141f5ddf0803d637217ee64fc152d
kreczko <lkreczko@googlemail.com>
2015-01-16 11:26:34 +0000

    CRAB3 configs for BLT unfolding production

---
+++ Configuration/unfolding_Summer11Leg/BLT/crab3/TTJets_MSDecays_scaleup_7TeV-madgraph_unfolding_NoSkim.py
@@ -0,0 +1,20 @@
+from WMCore.Configuration import Configuration
+from BristolAnalysis.NTupleTools.Configuration.BLT_unfold import input_files
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.General.requestName = 'TTJets_scaleup_7TeV_madgraph_BLTUnfold_NoSkim_TEST'
+config.General.transferLogs = True
+config.section_('JobType')
+config.JobType.psetName = 'BristolAnalysis/NTupleTools/test/unfoldingBLT_cfg.py'
+config.JobType.pluginName = 'Analysis'
+config.JobType.inputFiles = input_files
+
+config.JobType.pyCfgParams = ['useData=0', 'dataType=TTJets-scaleup', 'isTTbarMC=1', 'skim=NoSkim', 'CMSSW=53X', 'centreOfMassEnergy=7', 'storePDFWeights=0', 'applyType0METcorrection=1', 'applySysShiftCorrection=1']
+config.section_('Data')
+config.Data.inputDataset = '/TTJets_MSDecays_scaleup_TuneZ2star_7TeV-madgraph-tauola/Summer11LegDR-PU_S13_START53_LV6-v2/AODSIM'
+config.Data.unitsPerJob = 3
+config.Data.splitting = 'FileBased'
+config.section_('User')
+config.section_('Site')
+config.Site.storageSite = 'T2_UK_SGrid_Bristol'
