3ebcaf18780f7ad84aaf1f22e341ddb802c86b98
Emyr Clement <emyr.clement@bristol.ac.uk>
2015-06-02 15:49:29 +0100

    Update name of sample.

--- Configuration/crab3Configurations/PHYS14/QCD_bcToE_30to80_PU20bx25.py
+++ Configuration/crab3Configurations/PHYS14/QCD_bcToE_30to80_PU20bx25.py
@@ -1,6 +1,6 @@
 from BristolAnalysis.NTupleTools.commonConfig import config
 
 config.General.requestName = 'QCD_bcToE_30to80_PU20bx25'
-config.Data.inputDataset = '/QCD_Pt_30to80_bcToE_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
+config.Data.inputDataset = '/QCD_Pt_30to80_bcToE_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v2/MINIAODSIM'
 config.Data.unitsPerJob = 10
 config.Data.publishDataName = 'QCD_bcToE_30to80_PU20bx25'
