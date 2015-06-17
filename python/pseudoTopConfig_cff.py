ac8a4497e850f5e18a6fa8d41a08b6bd59f40822
Emyr Clement <emyr.clement@bristol.ac.uk>
2015-06-17 16:17:34 +0100

    Remove changes to parameters, now default in CMSSW.

--- python/pseudoTopConfig_cff.py
+++ python/pseudoTopConfig_cff.py
@@ -1,9 +1,3 @@
 def setupPseudoTop( process, cms ):
 	process.load( "TopQuarkAnalysis.TopEventProducers.producers.pseudoTop_cfi" )
-
-	# Default in CMSSW is 0.5 (relic from run 1)
-	process.pseudoTop.jetConeSize = cms.double(0.4)
-	# Current default is 80.2
-	process.pseudoTop.wMass = cms.double(80.4)
-
 	process.makePseudoTop = cms.Sequence( process.pseudoTop )
