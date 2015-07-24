4f771ec193658f5853e5d855a2a052702af7be9c
Emyr Clement <emyr.clement@bristol.ac.uk>
2015-07-24 11:12:03 +0100

    Update submission script.

--- python/submissionScripts/submitNTuples.py
+++ python/submissionScripts/submitNTuples.py
@@ -4,48 +4,64 @@ from shutil import move
 from os import remove
 import os
 
-version = '21'
+version = '22'
 crabWorkArea = 'workdirCrab/v'+version+'/'+str(date.today())
 print 'Using workdir ',crabWorkArea
 miniAodDir = 'BristolAnalysis/NTupleTools/Configuration/crab3Configurations/50ns/'
 jobConfigs = [
 
-# miniAodDir+'SingleMuon.py',
+miniAodDir+'SingleMuon.py',
 miniAodDir+'SingleElectron.py',
 
-# miniAodDir+'TTJets_PowhegPythia8.py',
-# miniAodDir+'DYJetsToLL_M50.py',
-# miniAodDir+'GJets_15to6000.py',
-# miniAodDir+'QCD_MuEnriched_1000toInf.py',
-# miniAodDir+'QCD_MuEnriched_120to170.py',
-# miniAodDir+'QCD_MuEnriched_15to20.py',
-# miniAodDir+'QCD_MuEnriched_170to300.py',
-# miniAodDir+'QCD_MuEnriched_20to30.py',
-# miniAodDir+'QCD_MuEnriched_300to470.py',
-# miniAodDir+'QCD_MuEnriched_30to50.py',
-# miniAodDir+'QCD_MuEnriched_470to600.py',
-# miniAodDir+'QCD_MuEnriched_50to80.py',
-# miniAodDir+'QCD_MuEnriched_600to800.py',
-# miniAodDir+'QCD_MuEnriched_800to1000.py',
-# miniAodDir+'QCD_MuEnriched_80to120.py',
-# miniAodDir+'TTJets_PowhegPythia6Tauola.py',
-# miniAodDir+'TTJets_PowhegPythia8_scaledown.py',
-# miniAodDir+'TTJets_PowhegPythia8_scaleup.py',
-# miniAodDir+'TTJets_PowhegPythia8_mtop1695.py',
-# miniAodDir+'TTJets_PowhegPythia8_mtop1755.py',
-# miniAodDir+'TTJets_amcatnloFXFX.py',
-# miniAodDir+'TTJets_amcatnloFXFX_mtop1695.py',
-# miniAodDir+'TTJets_amcatnloFXFX_mtop1755.py',
-# miniAodDir+'TTJets_amcatnloFXFX_scaledown.py',
-# miniAodDir+'TTJets_amcatnloFXFX_scaleup.py',
-# miniAodDir+'TTJets_madgraphMLM.py',
-# miniAodDir+'TToLeptons_t.py',
-# miniAodDir+'T_tW.py',
-# miniAodDir+'Tbar_tW.py',
-# miniAodDir+'WJetsToLNu.py',
+# miniAodDir + 'TTJets_PowhegPythia8.py',
 
-### 74X MC
-### 25 ns
+# miniAodDir + 'DYJetsToLL_M50.py',
+# miniAodDir + 'GJets_15to6000.py',
+# miniAodDir + 'QCD_EMEnriched_120to170.py',
+# miniAodDir + 'QCD_EMEnriched_15to20.py',
+# miniAodDir + 'QCD_EMEnriched_170to300.py',
+# miniAodDir + 'QCD_EMEnriched_20to30.py',
+# miniAodDir + 'QCD_EMEnriched_300toInf.py',
+# miniAodDir + 'QCD_EMEnriched_30to50.py',
+# miniAodDir + 'QCD_EMEnriched_50to80.py',
+# miniAodDir + 'QCD_EMEnriched_80to120.py',
+# miniAodDir + 'QCD_MuEnriched_1000toInf.py',
+# miniAodDir + 'QCD_MuEnriched_120to170.py',
+# miniAodDir + 'QCD_MuEnriched_15to20.py',
+# miniAodDir + 'QCD_MuEnriched_170to300.py',
+# miniAodDir + 'QCD_MuEnriched_20to30.py',
+# miniAodDir + 'QCD_MuEnriched_300to470.py',
+# miniAodDir + 'QCD_MuEnriched_30to50.py',
+# miniAodDir + 'QCD_MuEnriched_470to600.py',
+# miniAodDir + 'QCD_MuEnriched_50to80.py',
+# miniAodDir + 'QCD_MuEnriched_600to800.py',
+# miniAodDir + 'QCD_MuEnriched_800to1000.py',
+# miniAodDir + 'QCD_MuEnriched_80to120.py',
+# miniAodDir + 'QCD_bcToE_15to20.py',
+# miniAodDir + 'QCD_bcToE_170to250.py',
+# miniAodDir + 'QCD_bcToE_20to30.py',
+# miniAodDir + 'QCD_bcToE_250toInf.py',
+# miniAodDir + 'QCD_bcToE_30to80.py',
+# miniAodDir + 'QCD_bcToE_80to170.py',
+# miniAodDir + 'TTJets_PowhegPythia6Tauola.py',
+# miniAodDir + 'TTJets_PowhegPythia8_mtop1695.py',
+# miniAodDir + 'TTJets_PowhegPythia8_mtop1755.py',
+# miniAodDir + 'TTJets_PowhegPythia8_scaledown.py',
+# miniAodDir + 'TTJets_PowhegPythia8_scaleup.py',
+# miniAodDir + 'TTJets_amcatnloFXFX.py',
+# miniAodDir + 'TTJets_amcatnloFXFX_mtop1695.py',
+# miniAodDir + 'TTJets_amcatnloFXFX_mtop1755.py',
+# miniAodDir + 'TTJets_amcatnloFXFX_scaledown.py',
+# miniAodDir + 'TTJets_amcatnloFXFX_scaleup.py',
+# miniAodDir + 'TTJets_madgraphMLM.py',
+# miniAodDir + 'TToLeptons_t.py',
+# miniAodDir + 'T_tW.py',
+# miniAodDir + 'Tbar_tW.py',
+# miniAodDir + 'WJetsToLNu.py',
+
+
+# ## 74X MC
+# ## 25 ns
 # miniAodDir+'TTJets_amcatnloFXFX.py',
 # miniAodDir+'DYJetsToLL_M50.py',
 # miniAodDir+'QCD_EMEnriched_120to170.py',
