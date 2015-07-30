edd5b42eba9fe16e23865322fbc59519564c8465
Emyr Clement <emyr.clement@bristol.ac.uk>
2015-07-30 11:17:59 +0100

    Update json to most recent full golden.

--- Configuration/crab3Configurations/50ns/SingleElectron.py
+++ Configuration/crab3Configurations/50ns/SingleElectron.py
@@ -6,4 +6,4 @@ config.Data.inputDataset = '/SingleElectron/Run2015B-PromptReco-v1/MINIAOD'
 config.Data.splitting = 'EventAwareLumiBased'
 config.Data.unitsPerJob = 500000
 config.Data.publishDataName = 'SingleElectron'
-config.Data.lumiMask = '/hdfs/TopQuarkGroup/run2/json/Cert_246908-251252_13TeV_PromptReco_Collisions15_JSON.txt'
+config.Data.lumiMask = '/hdfs/TopQuarkGroup/run2/json/Cert_246908-251883_13TeV_PromptReco_Collisions15_JSON_v2.txt'
