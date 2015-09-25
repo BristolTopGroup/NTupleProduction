546bcc8e952ce8867bacc15d21368763b71fd318
kreczko <lkreczko@googlemail.com>
2015-09-25 14:35:55 +0100

    updated to newest JSON

--- Configuration/crab3Configurations/50ns/SingleElectron_PromptReco_Run2015C.py
+++ Configuration/crab3Configurations/50ns/SingleElectron_PromptReco_Run2015C.py
@@ -2,9 +2,8 @@ from BristolAnalysis.NTupleTools.commonConfig import config
 
 config.General.requestName = 'SingleElectron_PromptReco'
 config.JobType.pyCfgParams = ['isData=1']
-config.Data.inputDataset = '/SingleElectron/Run2015B-PromptReco-v1/MINIAOD'
+config.Data.inputDataset = '/SingleElectron/Run2015C-PromptReco-v1/MINIAOD'
 config.Data.splitting = 'EventAwareLumiBased'
-config.Data.runRange = '251643-251883'
 config.Data.unitsPerJob = 250000
 config.Data.publishDataName = 'SingleElectron_PromptReco'
 config.Data.lumiMask = '/hdfs/TopQuarkGroup/run2/json/Cert_246908-255031_13TeV_PromptReco_Collisions15_50ns_JSON_v2.txt'
--- Configuration/crab3Configurations/50ns/SingleMuon_ReReco_Run2015B.py
+++ Configuration/crab3Configurations/50ns/SingleMuon_ReReco_Run2015B.py
@@ -6,4 +6,4 @@ config.Data.inputDataset = '/SingleMuon/Run2015B-17Jul2015-v1/MINIAOD'
 config.Data.splitting = 'EventAwareLumiBased'
 config.Data.unitsPerJob = 250000
 config.Data.publishDataName = 'SingleMuon_Rereco'
-config.Data.lumiMask = '/hdfs/TopQuarkGroup/run2/json/Cert_246908-251883_13TeV_PromptReco_Collisions15_JSON_v2.txt'
+config.Data.lumiMask = '/hdfs/TopQuarkGroup/run2/json/Cert_246908-255031_13TeV_PromptReco_Collisions15_50ns_JSON_v2.txt'
