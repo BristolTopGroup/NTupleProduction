465b5b8e45b96ab6770d086abc2a3bac6dd9d03b
Douglas Burns <db0268@bristol.ac.uk>
2015-10-26 10:19:22 +0000

    get rid of old electron ids

--- python/ElectronID_cff.py
+++ python/ElectronID_cff.py
@@ -16,10 +16,7 @@ def setup_electronID(process, cms):
     # define which IDs we want to produce
     my_id_modules = [
                      'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Spring15_25ns_V1_cff',
-                     'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Spring15_50ns_V1_cff',
-                     # 'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring15_25ns_nonTrig_V1_cff',
                      'RecoEgamma.ElectronIdentification.Identification.mvaElectronID_Spring15_25ns_Trig_V1_cff',
-                    # 'RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_PHYS14_PU20bx25_V2_cff',
                     # 'RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV51_cff',
                     ]
                     
