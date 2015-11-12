8ffeaf8ec1c495459888515e5e12848dc5c6f3ed
Emyr Clement <emyr.clement@bristol.ac.uk>
2015-11-12 17:31:25 +0000

    Fix bug in PathOfInterest.  Also change names of non eta restricted triggers.

--- python/BristolNTuple_Trigger_cfi.py
+++ python/BristolNTuple_Trigger_cfi.py
@@ -21,16 +21,17 @@ nTupleTriggerEle23erWPLooseGsf = nTupleTrigger.clone( Prefix='HLTEle23erWPLooseG
 # Muon
 
 nTupleTriggerIsoMu18er = nTupleTrigger.clone( Prefix='HLTIsoMu18er.', PathOfInterest='HLT_IsoMu18_v', tightenTrigger = True, hltFilter = 'hltL3crIsoL1sMu16L1f0L2f10QL3f18QL3trkIsoFiltered0p09', cut = 'eta > -2.1 && eta < 2.1', minNumber = 1 )
-nTupleTriggerIsoMu20er = nTupleTrigger.clone( Prefix='HLTIsoMu20er.', PathOfInterest='HLT_IsoMu20_v')
-nTupleTriggerIsoTkMu20er = nTupleTrigger.clone( Prefix='HLTIsoTkMu20er.', PathOfInterest='HLT_IsoTkMu20_v')
+nTupleTriggerIsoMu20er = nTupleTrigger.clone( Prefix='HLTIsoMu20.', PathOfInterest='HLT_IsoMu20_v')
+nTupleTriggerIsoTkMu20er = nTupleTrigger.clone( Prefix='HLTIsoTkMu20.', PathOfInterest='HLT_IsoTkMu20_v')
 
 # MC
 nTupleTriggerEle27erWP75GsfMC = nTupleTrigger.clone( Prefix='HLTEle27erWP75GsfMC.', PathOfInterest='HLT_Ele27_eta2p1_WP75_Gsf_v')
-nTupleTriggerEle23erWP75GsfMC = nTupleTrigger.clone( Prefix='HLTEle23erWP75GsfMC.', PathOfInterest='HLT_Ele22_eta2p1_WP75_Gsf', tightenTrigger = True, hltFilter = 'hltSingleEle22WP75GsfTrackIsoFilter', cut = 'pt > 23', minNumber = 1 )
+nTupleTriggerEle23erWP75GsfMC = nTupleTrigger.clone( Prefix='HLTEle23erWP75GsfMC.', PathOfInterest='HLT_Ele22_eta2p1_WP75_Gsf_v', tightenTrigger = True, hltFilter = 'hltSingleEle22WP75GsfTrackIsoFilter', cut = 'pt > 23', minNumber = 1 )
+# nTupleTriggerEle23erWP75GsfMC = nTupleTrigger.clone( Prefix='HLTEle23erWP75GsfMC.', PathOfInterest='HLT_Ele22_eta2p1_WP75_Gsf_v' )
 
-nTupleTriggerIsoMu18erMC = nTupleTrigger.clone( Prefix='HLTIsoMu18erMC.', PathOfInterest='HLT_IsoMu17_eta2p1', tightenTrigger = True, hltFilter = 'hltL3crIsoL1sSingleMu16erL1f0L2f10QL3f17QL3trkIsoFiltered0p09', cut = 'pt > 18', minNumber = 1 )
-nTupleTriggerIsoMu20erMC = nTupleTrigger.clone( Prefix='HLTIsoMu20erMC.', PathOfInterest='HLT_IsoMu20_v')
-nTupleTriggerIsoTkMu20erMC = nTupleTrigger.clone( Prefix='HLTIsoTkMu20erMC.', PathOfInterest='HLT_IsoTkMu20_v')
+nTupleTriggerIsoMu18erMC = nTupleTrigger.clone( Prefix='HLTIsoMu18erMC.', PathOfInterest='HLT_IsoMu17_eta2p1_v', tightenTrigger = True, hltFilter = 'hltL3crIsoL1sSingleMu16erL1f0L2f10QL3f17QL3trkIsoFiltered0p09', cut = 'pt > 18', minNumber = 1 )
+nTupleTriggerIsoMu20MC = nTupleTrigger.clone( Prefix='HLTIsoMu20MC.', PathOfInterest='HLT_IsoMu20_v')
+nTupleTriggerIsoTkMu20MC = nTupleTrigger.clone( Prefix='HLTIsoTkMu20MC.', PathOfInterest='HLT_IsoTkMu20_v')
 
 
 
