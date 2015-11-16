83224e16d5e3b9de1d87587e69a036f9c7cf60a8
Douglas Burns <db0268@bristol.ac.uk>
2015-11-16 12:32:03 +0000

    New Lumi json. New scaleup/down datasets

--- Configuration/crab3Configurations/25ns/SingleMuon_PromptReco_RunD.py
+++ Configuration/crab3Configurations/25ns/SingleMuon_PromptReco_RunD.py
@@ -6,4 +6,5 @@ config.Data.inputDataset = '/SingleMuon/Run2015D-PromptReco-v4/MINIAOD'
 config.Data.splitting = 'EventAwareLumiBased'
 config.Data.unitsPerJob = 500000
 config.Data.outputDatasetTag = 'SingleMuon_PromptRecov4_RunD'
-config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-258750_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
+
