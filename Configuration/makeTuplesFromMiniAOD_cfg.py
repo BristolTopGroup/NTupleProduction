f84d3b95ed8b8d94fa456febc4295cc39f982fc3
Douglas Burns <db0268@bristol.ac.uk>
2016-02-10 09:04:44 +0000

    Tidy up makeTuplesFromMiniAOD, new GT

--- Configuration/makeTuplesFromMiniAOD_cfg.py
+++ Configuration/makeTuplesFromMiniAOD_cfg.py
@@ -58,19 +58,19 @@ globaltag = ''
 if (options.isData) : 
   globaltag='76X_dataRun2_v15' # ReReco+Prompt JECv6
 else :
-  globaltag = '74X_mcRun2_asymptotic_AllChannelsGood_v0' # 25ns MC
+  globaltag = '76X_mcRun2_asymptotic_v12' # 25ns MC
 
 print "Using Global Tag : ", globaltag
 process.GlobalTag.globaltag = cms.string(globaltag)
 process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
-
+# process.load('JetMETCorrections.Configuration.CorrectedJetProducersDefault_cff')
 
 # TT Gen Event configuration
 from BristolAnalysis.NTupleTools.ttGenConfig_cff import * 
 setupTTGenEvent( process, cms )
 
 # Particle level definitions
-from BristolAnalysis.NTupleTools.pseudoTopConfig_cff import *
+from BristolAnalysis.NTupleTools.pseudoTopConfig_cff import * #check if needed
 setupPseudoTop( process, cms )
 
 # Electron VID
@@ -78,8 +78,8 @@ from BristolAnalysis.NTupleTools.ElectronID_cff import *
 setup_electronID( process, cms )
 
 # Rerun HBHE filter and others
-# from BristolAnalysis.NTupleTools.metFilters_cfi import *
-# setupMETFilters( process, cms )
+from BristolAnalysis.NTupleTools.metFilters_cfi import *
+setupMETFilters( process, cms )
 
 # Rerun MET
 from BristolAnalysis.NTupleTools.MET_Setup_cff import setup_MET
@@ -139,47 +139,52 @@ process.nTupleTree.outputCommands.append( 'keep bool_topPairEPlusJetsSelectionTa
 process.nTupleTree.outputCommands.append( 'keep bool_topPairEPlusJetsQCDSelectionTagging_*FullSelection*_*' )
 process.nTupleTree.outputCommands.append( 'keep bool_topPairEPlusJetsConversionSelectionTagging_*FullSelection*_*' )
 
+
 if not options.isData:
-  # 25ns Triggers
+  # Remove 76X Data 25ns Triggers
   process.triggerSequence.remove( process.nTupleTriggerEle22erWPLooseGsf )
   process.triggerSequence.remove( process.nTupleTriggerIsoMu20 )
   process.triggerSequence.remove( process.nTupleTriggerIsoTkMu20 )
   process.triggerSequence.remove( process.nTupleTrigger )
+
+  # Delete removed modules 
   del process.nTupleTriggerEle22erWPLooseGsf, process.nTupleTriggerIsoMu20, process.nTupleTriggerIsoTkMu20, process.nTupleTrigger
 
-  # process.makingNTuples.remove( process.metFilters )
-  # del process.metFilters
-  pass
-
 else :
+  # Remove 76X MC 25ns Triggers
+  process.triggerSequence.remove( process.nTupleTriggerEle22erWPLooseGsfMC )
+  process.triggerSequence.remove( process.nTupleTriggerIsoMu20MC )
+  process.triggerSequence.remove( process.nTupleTriggerIsoTkMu20MC )
+  process.triggerSequence.remove( process.nTupleTrigger )
+
+  # Remove PseudoTop and MC Gen Variables
   process.makingNTuples.remove( process.makePseudoTop )
   process.nTuples.remove( process.pseudoTopSequence )
   process.nTuples.remove( process.nTupleGenMET )
   process.nTuples.remove( process.nTupleGenJets )
   process.nTuples.remove( process.nTupleGenEventInfo )
   process.nTuples.remove( process.nTupleGenParticles )
-  process.nTupleTree.outputCommands.append('drop *_nTuplePFJets_*Gen*_*')
-  del process.makePseudoTop, process.pseudoTopSequence, process.nTupleGenMET
-  del process.nTupleGenJets,  process.nTupleGenEventInfo, process.nTupleGenParticles
 
-  # 25ns Triggers
-  process.triggerSequence.remove( process.nTupleTriggerEle22erWPLooseGsfMC )
-  process.triggerSequence.remove( process.nTupleTriggerIsoMu20MC )
-  process.triggerSequence.remove( process.nTupleTriggerIsoTkMu20MC )
-  process.triggerSequence.remove( process.nTupleTrigger )
+  # Do not keep Gen branches
+  process.nTupleTree.outputCommands.append('drop *_nTuplePFJets_*Gen*_*')
+
+  # Delete removed modules and sequences (So they do not run on unscheduled)
+  del process.makePseudoTop, process.pseudoTopSequence, process.pseudoTop
+  del process.nTuplePseudoTopJets, process.nTuplePseudoTopLeptons, process.nTuplePseudoTopNeutrinos, process.nTuplePseudoTops
+  del process.nTupleGenMET, process.nTupleGenJets,  process.nTupleGenEventInfo, process.nTupleGenParticles
   del process.nTupleTriggerEle22erWPLooseGsfMC, process.nTupleTriggerIsoMu20MC, process.nTupleTriggerIsoTkMu20MC, process.nTupleTrigger
 
-# if options.isData and options.isRereco:
+# 76X datasets are all ReReco so far
+process.nTupleEvent.metFiltersInputTag = cms.InputTag('TriggerResults','','PAT')
+
+# if options.isData and options.isReReco:
 #   process.nTupleEvent.metFiltersInputTag = cms.InputTag('TriggerResults','','PAT')
-
-
-# if options.isData and options.isRunC:
-# 	process.triggerSequence.remove( process.nTupleTriggerIsoMu18 )
-# 	del process.nTupleTriggerIsoMu18
 
 if not options.isTTbarMC:
   process.makingNTuples.remove( process.ttGenEvent )
   process.selectionCriteriaAnalyzer.genSelectionCriteriaInput = cms.VInputTag()
+  del process.ttGenEvent
+
 else:
   process.nTupleGenEventInfo.isTTbarMC = cms.bool( True )
 
