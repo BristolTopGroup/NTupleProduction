509ee4ff3cb40f3ab26a6572d07e483018b59d24
Douglas Burns <db0268@bristol.ac.uk>
2016-03-10 15:02:08 +0000

    ntuple v11

--- python/submissionScripts/killNtuples.py
+++ python/submissionScripts/killNtuples.py
@@ -10,7 +10,7 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-pathOfCrabWorkdirs = '/storage/db0268/TopCrossSections/NTupleProduction/CMSSW_7_6_3_patch2/src/workdirCrab/v7/2016-02-11/'
+pathOfCrabWorkdirs = '/storage/db0268/TopCrossSections/NTupleProduction/CMSSW_7_6_3_patch2/src/workdirCrab/v11/2016-03-10/'
 
 for crabWorkdir in os.listdir(pathOfCrabWorkdirs):
 	os.system('crab kill ' + pathOfCrabWorkdirs+crabWorkdir )
