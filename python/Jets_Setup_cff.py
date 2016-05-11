de5e1a72ed2ff2cd9f3a40cefb15756b97410795
kreczko <lkreczko@googlemail.com>
2016-05-11 15:10:56 +0100

    fixing jetUpdater imports for CMSSW 8X

--- python/Jets_Setup_cff.py
+++ python/Jets_Setup_cff.py
@@ -76,73 +76,16 @@ def setup_jets(process, cms, options, postfix="PFlow"):
     print 'PF Jets'
     print inputJetCorrLabel
 
-    from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import patJetCorrFactorsUpdated
-    process.patJetCorrFactorsReapplyJEC = patJetCorrFactorsUpdated.clone(
+    from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import updatedPatJetCorrFactors
+    process.patJetCorrFactorsReapplyJEC = updatedPatJetCorrFactors.clone(
       src = cms.InputTag("slimmedJets"),
       levels = inputJetCorrLabel[1],
       payload = inputJetCorrLabel[0] ) # Make sure to choose the appropriate levels and payload here!
 
-    from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import patJetsUpdated
-    process.patJetsReapplyJEC = patJetsUpdated.clone(
+    from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import updatedPatJets
+    process.patJetsReapplyJEC = updatedPatJets.clone(
       jetSource = cms.InputTag("slimmedJets"),
       jetCorrFactorsSource = cms.VInputTag(cms.InputTag("patJetCorrFactorsReapplyJEC"))
       )
 
     process.reapplyJEC = cms.Sequence( process.patJetCorrFactorsReapplyJEC + process.patJetsReapplyJEC )
-
-#     if options.CMSSW == '44X':
-#         process.patJetCorrFactorsPFlow.payload = inputJetCorrLabel[0]
-#         process.patJetCorrFactorsPFlow.levels = inputJetCorrLabel[1]
-#         process.patJetCorrFactorsPFlow.rho = cms.InputTag("kt6PFJets", "rho")
-#
-#     ###############################
-#     #### Jet RECO includes ########
-#     ###############################
-#
-#     from RecoJets.JetProducers.SubJetParameters_cfi import SubJetParameters
-#
-#     ###############################
-#     ###### Bare KT 0.6 jets #######
-#     ###############################
-#
-#     from RecoJets.JetProducers.kt4PFJets_cfi import kt4PFJets
-#
-#     process.kt6PFJets = kt4PFJets.clone(
-#                                         rParam=cms.double(0.6),
-#                                         doAreaFastjet=cms.bool(True),
-#                                         doRhoFastjet=cms.bool(True)
-#     )
-#     process.kt6PFJetsPFlow = kt4PFJets.clone(
-#                                              rParam=cms.double(0.6),
-#                                              src=cms.InputTag('pfNoElectron' + postfix),
-#                                              doAreaFastjet=cms.bool(True),
-#                                              doRhoFastjet=cms.bool(True)
-#     )
-#     process.kt4PFJetsPFlow = kt4PFJets.clone(
-#                                              rParam=cms.double(0.4),
-#                                              src=cms.InputTag('pfNoElectron' + postfix),
-#                                              doAreaFastjet=cms.bool(True),
-#                                              doRhoFastjet=cms.bool(True)
-#     )
-#
-#     ###############################
-#     ### TagInfo and Matching Setup#
-#     ###############################
-#
-# Do some configuration of the jet substructure things
-#     for jetcoll in [process.patJetsPFlow] :
-#         if options.useData == False :
-#             jetcoll.embedGenJetMatch = True
-#             jetcoll.getJetMCFlavour = True
-#             jetcoll.addGenPartonMatch = True
-# Add CATopTag info... piggy-backing on b-tag functionality
-#         jetcoll.addBTagInfo = True
-#         jetcoll.embedCaloTowers = True
-#         jetcoll.embedPFCandidates = True
-#
-#     additionalJets = [getattr(process, "kt6PFJets"),
-#                       getattr(process, "kt6PFJets" + postfix),
-#                       getattr(process, "kt4PFJets" + postfix)]
-#     pfNoElectron = getattr(process, "pfNoElectron" + postfix)
-#     for module in additionalJets :
-#         getattr(process, "patPF2PATSequence" + postfix).replace(pfNoElectron, pfNoElectron * module)
