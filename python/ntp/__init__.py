f4e0ff9283a7b3131541d148a83a8f16b589a668
Luke Kreczko <lkreczko@googlemail.com>
2016-05-17 16:16:57 +0100

    added safety net for loggin if workspace does not exist

--- python/ntp/__init__.py
+++ python/ntp/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 import os
+import sys
 import logging
 
 __version__ = '0.0.1'
@@ -7,15 +8,22 @@ current_path = os.path.split(__file__)[0]
 path_to_base = os.path.join(current_path, '..')
 __path__.append(path_to_base)
 
+if not 'NTPROOT' in os.environ:
+    print("Could not find environmental variable 'NTPROOT'")
+    print("You need to run 'source bin/env.sh' first!")
+    sys.exit(-1)
+NTPROOT = os.environ['NTPROOT']
+
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.INFO)
 # logging to a file
-NTPROOT = os.environ['NTPROOT']
-formatter = logging.Formatter('%(asctime)s [%(name)s]  %(levelname)s: %(message)s')
-fh = logging.FileHandler(NTPROOT + '/workspace/log/ntp.log')
-fh.setFormatter(formatter)
-fh.setLevel(logging.DEBUG)
-LOG.addHandler(fh)
+formatter = logging.Formatter(
+    '%(asctime)s [%(name)s]  %(levelname)s: %(message)s')
+if os.path.exists(NTPROOT + '/workspace/log'):
+    fh = logging.FileHandler(NTPROOT + '/workspace/log/ntp.log')
+    fh.setFormatter(formatter)
+    fh.setLevel(logging.DEBUG)
+    LOG.addHandler(fh)
 
 # logging to the console
 formatter = logging.Formatter('%(message)s')
