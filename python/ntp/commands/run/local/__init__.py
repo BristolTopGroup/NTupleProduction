d8f128cdb14297e48c6b678f50e4eb95ab9cef7b
kreczko <lkreczko@googlemail.com>
2016-05-24 12:12:04 +0100

    Console: fix for datasets with more than 255 files

--- python/ntp/commands/run/local/__init__.py
+++ python/ntp/commands/run/local/__init__.py
@@ -47,8 +47,14 @@ process.maxEvents.input = cms.untracked.int32({nevents})
 
 process.TFileService.fileName=cms.string('{OUTPUT_FILE}')
 
+# In order to work around the limitation of 255 parameters to a python function
+# we pass 1 tuple of all the files to the vstring function
+# see
+# https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuidePoolInputSources#Example_3_More_than_255_input_fi
 process.source.fileNames = cms.untracked.vstring(
-{input_files}
+*(
+{input_files},
+)
 )
 
 process.nTuplePFJets.btagCalibrationFile = cms.string('{BTAG_CALIB_FILE}')
