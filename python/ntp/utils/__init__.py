74cb5085ecf799c0fb4ceecda1580c2c967bda9f
kreczko <lkreczko@googlemail.com>
2016-09-05 12:39:56 +0100

    fixed bugs in make_even_chunks

--- python/ntp/utils/__init__.py
+++ python/ntp/utils/__init__.py
@@ -2,18 +2,28 @@
     A small collections of functions that are useful in more than one place.
     All of this should move to DailyPythonScripts.
 """
+from __future__ import division
 
 
-def make_even_chunks(list_of_things, n_chunks):
+def make_even_chunks(list_of_things, size_of_chunk):
     """
-        Creates even chunks given a list and the number of chunks
+        Creates even chunks given a list and the number of chunks.
+        @param: list_of_things: the list to be chunked
+        @param: size_of_chunk: the minimal size of a chunk.
+        This method will re-distribute items if the last chunk is smaller than
+        size_of_chunks.
+        E.g. list_of_things = list(range(100)) and size_of_chunk = 9
+        will give you 10 groups where one of them has 10 and all others have 9.
     """
+    n_items = len(list_of_things)
+    size_of_chunk = min(size_of_chunk, n_items)
+    n = size_of_chunk
+    n_groups = int(n_items / n)
+
     last = 0
-    n = n_chunks
-    l = list_of_things
-    for i in range(1, n + 1):
-        cur = int(round(i * (len(l) / n)))
-        yield l[last:cur]
+    for i in range(1, n_groups + 1):
+        cur = int(round(i * n_items / n_groups))
+        yield list_of_things[last:cur]
         last = cur
 
 
