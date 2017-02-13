604e02cec665d1c28ccc5e711f987b79797c83bd
DouglasBurns <db0268@bristol.ac.uk>
2017-02-13 13:26:10 +0100

    Remove egmID merge, run regression form online db, update version (#295)

--- python/crab/base.py
+++ python/crab/base.py
@@ -15,7 +15,7 @@ if not 'HEP_PROJECT_ROOT' in os.environ:
     sys.exit(-1)
 
 # this is the NTupleVersion!
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 NTPROOT = os.environ['HEP_PROJECT_ROOT']
 WORKSPACE = os.path.join(NTPROOT, 'workspace')
 CRAB_WS = os.path.join(WORKSPACE, 'crab')
