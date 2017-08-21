a2794846da42ab158adefe023b4ac2cc18870568
Emyr Clement <emyr.clement@bristol.ac.uk>
2017-08-21 11:13:04 +0100

    Set beta to 2 for nJettiness calculation.

--- python/nJettiness_cff.py
+++ python/nJettiness_cff.py
@@ -5,7 +5,7 @@ from RecoJets.JetProducers.nJettinessAdder_cfi import Njettiness
 njettiness = Njettiness.clone(
     src = cms.InputTag('nTuplePFCands', 'OneJet'),
 	measureDefinition = cms.uint32(4),
-	beta = cms.double(1.0),
+	beta = cms.double(2.0),
 	R0 = cms.double( 0.4 ),
 	Rcutoff = cms.double( 0.4 ),
 	Njets=cms.vuint32(1,2,3,4,5,6),
